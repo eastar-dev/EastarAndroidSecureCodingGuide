4e01f0b2ed0556eb060d826b34310cebc540684e
eastar <pluto@eastandroid.com>
2021-06-29 17:35:56 +0900

    TextView 사용법

--- app/src/main/java/dev/eastar/eastarandroidsecurecodingguide/TextViewActivity.kt
+++ app/src/main/java/dev/eastar/eastarandroidsecurecodingguide/TextViewActivity.kt
@@ -1,19 +1,37 @@
 package dev.eastar.eastarandroidsecurecodingguide
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import dev.eastar.eastarandroidsecurecodingguide.databinding.TextviewActivityBinding
+import java.nio.ByteBuffer
+import java.nio.CharBuffer
+import java.nio.charset.Charset
 
 class TextViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bb = TextviewActivityBinding.inflate(layoutInflater)
         setContentView(bb.root)
-        bb.setTextButton.setOnClickListener {
 
+        bb.setTextButton1.setOnClickListener {
+            val charSampleData: CharArray = charArrayOf('e', 'a', 's', 't', 'a', 'r', 'd', 'e', 'v', '1')
+            bb.sampleTextView.setText(charSampleData, 0, 10)
         }
-        bb.removeTextButton.setOnClickListener {
 
+        bb.setTextButton2.setOnClickListener {
+            val charSampleData = "eastardev2"
+            bb.sampleTextView.text = charSampleData
+        }
+
+        bb.setTextButton3.setOnClickListener {
+            val charSampleData = byteArrayOf('e'.code.toByte(), 'a'.code.toByte(), 's'.code.toByte(), 't'.code.toByte(), 'a'.code.toByte(), 'r'.code.toByte(), 'd'.code.toByte(), 'e'.code.toByte(), 'v'.code.toByte(), '3'.code.toByte())
+            bb.sampleTextView.text = String(charSampleData)
+        }
+
+        bb.removeTextButton.setOnClickListener {
+            bb.sampleTextView.text = ""
         }
     }
 }
